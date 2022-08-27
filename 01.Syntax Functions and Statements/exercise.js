312ade38cf354edc2043ea9a565f417f68e1f5d0
Bobogamen <b_davidov@abv.bg>
2022-08-27 17:59:00 +0300

    test commit

--- 01.Syntax Functions and Statements/exercise.js
+++ 01.Syntax Functions and Statements/exercise.js
@@ -1,1 +1,13 @@
-//1.
+//1.Fruit
+
+function buyingFruit(name, weight, price,) {
+
+      let money = price * weight / 1000;
+      weight = weight / 1000;
+
+      console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${name}.`)
+
+}
+
+// buyingFruit('orange', 2500, 1.80);
+// buyingFruit('apple', 1563, 2.35);
