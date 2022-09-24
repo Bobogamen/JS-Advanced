8d2f9601a1c7aa17fed47eea1ab383143bcde51e
Bobogamen <b_davidov@abv.bg>
2022-09-24 06:45:33 +0300

    allmost done with exercises

--- 03.Objects and Composition/exercise.js
+++ 03.Objects and Composition/exercise.js
@@ -112,29 +112,125 @@ function carFactory(order) {
 
 function heroicInventory(heroes) {
 
-      let result = [
-
-      ];
+      let result = [];
 
       for (const hero of heroes) {
-            let current = {};
-            current.name = hero.split(' / ')[0];
-            current.level = hero.split(' / ')[1];
+            let [name, level, items] = hero.split(' / ');
+            level = Number(level);
+            items = items ? items.split(', ') : [];
 
-            let currentItems = [];
-            for (const item of hero.split(', ')[2]) {
-                  currentItems.push(item);
-            }
-
-            current.items = currentItems
-
-            result.push(current);
+            result.push({ name, level, items })
       }
-
-      console.log(result);
-
+      console.log(JSON.stringify(result));
 };
 
-heroicInventory(['Isacc / 25 / Apple, GravityGun',
-      'Derek / 12 / BarrelVest, DestructionSword',
-      'Hes / 1 / Desolator, Sentinel, Antara'])
+// heroicInventory(['Isacc / 25 / Apple, GravityGun',
+//       'Derek / 12 / BarrelVest, DestructionSword',
+//       'Hes / 1 / Desolator, Sentinel, Antara']);
+
+// heroicInventory(['Jake / 1000 / Gauss, HolidayGrenade']);
+
+
+//5. Lowest Prices in Cities
+
+function lowestPriceInCities(input) {
+
+      let result = {};
+
+      for (const element of input) {
+            let [town, product, price] = element.split(' | ');
+            price = Number(price);
+
+            if (!result.hasOwnProperty(product)) {
+                  result[product] = {};
+                  result[product][town] = price;
+            } else {
+                  if (price < Object.values(result[product])) {
+                        let oldTown = Object.keys(result[product]);
+                        result[product][town] = price;
+
+                        delete result[product][oldTown];
+                  }
+            }
+      };
+
+      // console.log(Object.entries(result));
+
+      for (const element in result) {
+            console.log(`${element} -> ${Object.values(result[element])} (${Object.keys(result[element])})`);
+      }
+}
+
+
+// lowestPriceInCities(['Sample Town | Sample Product | 1000',
+//       'Sample Town | Orange | 2',
+//       'Sample Town | Peach | 1',
+//       'Sofia | Orange | 3',
+//       'Sofia | Peach | 2',
+//       'New York | Sample Product | 1000.1',
+//       'New York | Burger | 10']);
+
+// lowestPriceInCities(['Sofia City | Audi | 100000',
+//       'Sofia City | BMW | 100000',
+//       'Sofia City | Mitsubishi | 10000',
+//       'Sofia City | Mercedes | 10000',
+//       'Sofia City | NoOffenseToCarLovers | 0',
+//       'Mexico City | Audi | 1000',
+//       'Mexico City | BMW | 99999',
+//       'Mexico City | Mitsubishi | 10000',
+//       'New York City | Mitsubishi | 1000',
+//       'Washington City | Mercedes | 1000']);
+
+
+//6. Store Catalogue 
+
+function storeCatalogue(input) {
+
+      let result = {};
+
+      for (const element of input) {
+            let [name, price] = element.split(' : ');
+            price = Number(price);
+
+            let letter = name.charAt();
+
+            if (!result.hasOwnProperty(letter)) {
+                  result[letter] = {};
+                  result[letter][name] = price;
+            } else {
+                  result[letter][name] = price;
+            }
+      }
+
+      Object.values(result).forEach(element => {
+            console.log(element);
+      });
+
+
+
+      for (const element in result) {
+            console.log(element);
+            console.log(`  ${Object.entries(result[element])}`);
+      }
+
+
+
+}
+
+storeCatalogue(['Appricot : 20.4',
+      'Fridge : 1500',
+      'TV : 1499',
+      'Deodorant : 10',
+      'Boiler : 300',
+      'Apple : 1.25',
+      'Anti-Bug Spray : 15',
+      'T-Shirt : 10']);
+
+// storeCatalogue(['Banana : 2',
+// 'Rubic \'s Cube : 5',
+// 'Raspberry P : 4999',
+// 'Rolex : 100000',
+// 'Rollon : 10',
+// 'Rali Car : 2000000',
+// 'Pesho : 0.000001',
+// 'Barrel : 10']);
