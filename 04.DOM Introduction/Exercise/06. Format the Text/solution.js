47d3bccaa045e3f433323ff962aa20694dae7ad5
Computer <b_davidov@abv.bg>
2022-10-04 07:29:22 +0300

    work...

--- 04.DOM Introduction/Exercise/06. Format the Text/solution.js
+++ 04.DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value.split('.');
+  let output = document.getElementById('output');
+
+  let sentances = input.filter((p) => p.length > 0);
+
+  for (let i = 0; i < sentances.length; i += 3) {
+    let paragrapgh = [];
+
+    for (let j = 0; j < 3; j++) {
+      if (sentances[i + j]) {
+        paragrapgh.push(sentances[i + j]);
+      }
+    }
+
+    let result = paragrapgh.join('.')  + '.';
+    output.innerHTML += (`<p>${result}</p>`);
+  }
 }
--- 04.DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ 04.DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
-      
+      let input = JSON.parse(document.getElementById("inputs").children[1].value);
+
+      let output = [];
+
+      for (const item of input) {
+
+         let restaurant = item.split(" - ")[0];
+         let workers = item.split(" - ")[1];
+         
+         debugger;
+      }
+
+
+
    }
 }
