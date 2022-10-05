9a13d4cec8bb5c573219659d72528079d89f937d
Bobogamen <b_davidov@abv.bg>
2022-10-06 02:19:13 +0300

    DOM Introduction finished

--- 04.DOM Introduction/Exercise/08. Generate Report/report.js
+++ 04.DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,31 @@
 function generateReport() {
-    //TODO
+    let columns = document.querySelectorAll("input");
+    let rows = document.querySelectorAll("tbody tr");
+
+    let checkedColumns = [];
+
+    let result = [];
+
+    for (let i = 0; i < columns.length; i++) {
+        if (columns[i].checked) {
+            checkedColumns.push(i);
+            checkedColumns.push(columns[i].name);
+        }
+    }
+
+    for (const line of rows) {
+
+        let current = {};
+
+        for (let i = 0; i < checkedColumns.length; i += 2) {
+            let key = checkedColumns[i + 1];
+            let value = line.children[checkedColumns[i]].textContent;
+
+            current[key] = value;
+        }
+
+        result.push(current);
+        debugger;
+    }
+    document.getElementById("output").textContent = JSON.stringify(result);
 }
