a73620b6a1b92fe37c5968ef30dcf09f6eab363d
Computer <b_davidov@abv.bg>
2022-10-11 13:40:26 +0300

    DOM Lab done

--- 05.DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ 05.DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-   //TODO...
+   let addButtons = document.getElementsByTagName('button');
+   let checkOut = document.getElementsByClassName('checkout')[0];
+   let textArea = document.getElementsByTagName('textarea')[0];
+
+   let prodcuts = [];
+   let sum = 0;
+
+   for (let i = 0; i < addButtons.length; i++) {
+      if (i == 3) {
+         checkOut.addEventListener('click', (event) => checkOutCart(event));
+         break;
+      }
+
+      let productName = addButtons[i].parentElement.parentElement.children[1].children[0].textContent;
+      let productPrice = addButtons[i].parentElement.parentElement.children[3].textContent;
+      addButtons[i].addEventListener('click', (event) => add(event, productName, productPrice));
+
+   }
+
+
+   function add(event, name, price) {
+      if (!prodcuts.includes(name)) {
+         prodcuts.push(name);
+      }
+
+      sum += Number(price);
+      textArea.textContent += `Added ${name} for ${price} to the cart.\n`
+   }
+
+
+
+   function checkOutCart(event) {
+      for (const button of addButtons) {
+         button.disabled = true;
+      }
+
+         textArea.textContent += `You bought ${prodcuts.join(", ")} for ${sum.toFixed(2)}.`
+   }
 }
