7e388069b769f7d6f1e666969fc7e26c23dbb590
Bobogamen <b_davidov@abv.bg>
2022-11-23 21:25:41 +0200

    meme-lounge

---
+++ Applications/exam-prep/meme-lounge/src/views/details.js
@@ -0,0 +1,42 @@
+import { deleteMeme, getMemeById } from '../api/memes.js';
+import { html } from '../lib.js';
+import { getUserData } from '../util.js';
+
+const detailsTemplate = (meme, isOwner, onDelete) => html`
+<section id="meme-details">
+        <h1>Meme Title: ${meme.title}
+        </h1>
+        <div class="meme-details">
+            <div class="meme-img">
+                <img alt="meme-alt" src="${meme.imageUrl}">
+            </div>
+            <div class="meme-description">
+                <h2>Meme Description</h2>
+                <p>
+                    ${meme.description}
+                </p>
+
+                ${isOwner ? html`
+                <a class="button warning" href="/edit/${meme._id}">Edit</a>
+                <button @click=${onDelete} class="button danger">Delete</button>` : ''}
+                </div>
+            </div>
+</section>`;
+
+export async function detailsView(ctx) {
+    const memeId = ctx.params.id
+    const meme = await getMemeById(memeId);
+
+    const isOwner = getUserData()?.id == meme._ownerId;
+
+    ctx.render(detailsTemplate(meme, isOwner, onDelete));
+
+    async function onDelete() {
+        const choice = confirm('Are you sure you want to delete this meme?')
+
+        if (choice) {
+            await deleteMeme(memeId)
+            ctx.page.redirect('/memes');
+        }
+    }
+}
